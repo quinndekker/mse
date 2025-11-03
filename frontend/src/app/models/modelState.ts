70e598a413e148ac6bfe034d15c5c5f8b4a10608
Quinn Dekker <dekker8491@uwlax.edu>
2025-11-03 12:47:12 +0000

    model update pt 2

--- frontend/src/app/models/modelState.ts
+++ frontend/src/app/models/modelState.ts
@@ -2,17 +2,17 @@ import { Prediction } from '../models/prediction';
 type Timeframe = '1d' | '2w' | '2m';
 import { ModelDetails } from './modelDetails';
 export interface ModelState {
-    sector: string;
-    timeframe: Timeframe;
-    loading: boolean;
-    error?: string | null;
-    data?: ModelDetails | null;
-  
-    predictions?: Prediction[] | null;
-    predLoading?: boolean;
-    predError?: string | null;
-  
-    // NEW
-    avgAccuracy?: number | null;  // mean of valid accuracies
-    accSampleSize?: number;       // how many predictions had accuracy
-  }
+  sector: string;
+  timeframe: '1d' | '2w' | '2m';
+
+  loading?: boolean;
+  data?: any | null;
+  error?: string | null;
+
+  predLoading?: boolean;
+  predError?: string | null;
+  predictions?: Prediction[] | null;
+
+  // NEW
+  avgMSE?: number | null;
+}
